d8e5ca359721a7e5c55d9f766e9ff03b787415d3
DagHemphal <96533741+DagHemphal@users.noreply.github.com>
2021-12-28 18:17:19 +0100

    fixed path for soruces glitch in chrome

--- static/js/app.js
+++ static/js/app.js
@@ -91,7 +91,7 @@ $(document).ready(function(e) {
 		
 	});
 
-	$.get( "api/get_games", function( data ) {
+	$.get( "./api/get_games", function( data ) {
 		console.log(data);
 		var select = $("#game");
 		var ret;
@@ -118,7 +118,7 @@ $(document).ready(function(e) {
 
 		$.ajax({
 		    type: 'POST',
-		    url: 'api/join',
+		    url: './api/join',
 		    data: JSON.stringify(data),
 		    success: function( data ) {
 			  console.log(data);
@@ -172,7 +172,7 @@ $(document).ready(function(e) {
 			console.log(data);
 	  		$.ajax({
 			    type: 'POST',
-			    url: 'api/done',
+			    url: './api/done',
 			    data: JSON.stringify (data),
 			    success: function(data) { 
 			    	console.log(data);
